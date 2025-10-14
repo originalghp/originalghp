ce805bc9414f188c42c60868adde3107dc270bbe
originalghp <original.gagi88@gmail.com>
2025-10-14 16:45:29 -0300

    Add files via upload

--- Php/ejer18FormToJsonAPIfetch/index.php
+++ Php/ejer18FormToJsonAPIfetch/index.php
@@ -1,18 +1,25 @@
 <?php
-// Leer datos del formulario (PDF PHP Parte 1 - Página 13)
-$codigoUsuario = $_POST['codigoUsuario'];
-$apellidoUsuario = $_POST['apellidoUsuario'];
-$nombreUsuario = $_POST['nombreUsuario'];
+// Verificar que se recibieron los datos (PDF PHP Parte 2 - Página 1)
+if (isset($_POST['codigoUsuario']) && isset($_POST['apellidoUsuario']) && isset($_POST['nombreUsuario'])) {
+    
+    // Leer datos del formulario (PDF PHP Parte 1 - Página 13)
+    $codigoUsuario = $_POST['codigoUsuario'];
+    $apellidoUsuario = $_POST['apellidoUsuario'];
+    $nombreUsuario = $_POST['nombreUsuario'];
 
-// Crear objeto con los datos (PDF PHP Parte 1 - Página 9)
-$objUsuario = new stdClass();
-$objUsuario->RegistroCodUsuario = $codigoUsuario;
-$objUsuario->RegistroApellido = $apellidoUsuario;
-$objUsuario->RegistroNombre = $nombreUsuario;
+    // Crear objeto con los datos (PDF PHP Parte 1 - Página 9)
+    $objUsuario = new stdClass();
+    $objUsuario->RegistroCodUsuario = $codigoUsuario;
+    $objUsuario->RegistroApellido = $apellidoUsuario;
+    $objUsuario->RegistroNombre = $nombreUsuario;
 
-// Convertir a JSON (PDF PHP Parte 1 - Página 12)
-$jsonUsuario = json_encode($objUsuario);
+    // Convertir a JSON (PDF PHP Parte 1 - Página 12)
+    $jsonUsuario = json_encode($objUsuario);
 
-// Mostrar JSON (PDF PHP Parte 1 - Página 12)
-echo $jsonUsuario;
+    // Mostrar JSON (PDF PHP Parte 1 - Página 12)
+    echo $jsonUsuario;
+    
+} else {
+    echo "Error: No se recibieron los datos del formulario";
+}
 ?>
